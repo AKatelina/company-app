c9d2fba8d8eac9905a36e60a988607d89a4db732
AKatelinskaya <anastasiakatelinskaya@gmail.com>
2023-03-07 14:13:13 +0300

    settings open close mobile menu

--- js/main.js
+++ js/main.js
@@ -1,21 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    //открытие мобильного меню
-    document.querySelectorAll('.navbar__mb-btn-open-sidebar').forEach(function (mbMenuBtn) {
-        mbMenuBtn.addEventListener('click', function () {
-            document.querySelectorAll('.sidebar').forEach(function (sidebar) {
+
+    //открытие и закрытие мобильного меню
+    document.querySelectorAll('.app').forEach(function (app) {
+        app.addEventListener('click', function (event) {
+            const sidebar = document.querySelector(".sidebar");
+            const isSidebarOpen = sidebar.classList.contains("sidebar__mb-open");
+
+            const openMBtn = document.querySelector(".navbar__mb-btn-open-sidebar");
+            const closeMBtn = document.querySelector(".sidebar__mb-btn-close-sidebar");
+
+            const target = event.target;
+
+            console.log(closeMBtn.contains(target));
+            if ((isSidebarOpen && !sidebar.contains(target)) //попала по правому пространству
+                || (!isSidebarOpen && openMBtn.contains(target)) //попала по мобильному меню
+                || (isSidebarOpen && closeMBtn.contains(target))) { //попала по кнопке закрыть
                 sidebar.classList.toggle('sidebar__mb-open')
-            })
-        })
+            }
+        });
     });
 
-    //закрытие мобильного меню
-    document.querySelectorAll('.sidebar__mb-btn-close-sidebar').forEach(function (mbMenuBtn) {
-        mbMenuBtn.addEventListener('click', function () {
-            document.querySelectorAll('.sidebar').forEach(function (sidebar) {
-                sidebar.classList.toggle('sidebar__mb-open')
-            })
-        })
-    });
+    // //закрытие мобильного меню
+    // document.querySelectorAll('.sidebar__mb-btn-close-sidebar').forEach(function (mbMenuBtn) {
+    //     mbMenuBtn.addEventListener('click', function () {
+    //         document.querySelectorAll('.sidebar').forEach(function (sidebar) {
+    //             sidebar.classList.toggle('sidebar__mb-open')
+    //         })
+    //     })
+    // });
 
     //открытие меню филиалов
     document.querySelectorAll('.sidebar__current-filial').forEach(function (chooseFilialBtn) {
